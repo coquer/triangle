a99e0260a6739bcd02452d032f45d44a53164fef
Jorge Y. C. Rodriguez <self@jorgerodriguez.dk>
2018-08-30 23:25:35 +0200

    Small changes

--- js/script.js
+++ js/script.js
@@ -4,53 +4,51 @@ btnGetTriangle.addEventListener("click", getTriangle);
 
 //create triangle type and validate it
 function getTriangle() {
-
-    var nr1, nr2, nr3, result;
-
     //get the number values from the input fields
-    nr1 = parseFloat(document.getElementById('nr1').value);
-    nr2 = parseFloat(document.getElementById('nr2').value);
-    nr3 = parseFloat(document.getElementById('nr3').value);
+    var nr1 = parseFloat(document.getElementById('nr1').value);
+    var nr2 = parseFloat(document.getElementById('nr2').value);
+    var nr3 = parseFloat(document.getElementById('nr3').value);
     
-    result = document.getElementById('result');
+    var result = document.getElementById('result');
      
     //check if input fields are empty, check if they are less and equal to 0
-    if(isNaN(nr1) || isNaN(nr2) || isNaN(nr3) || (nr1 == null || nr1 == "") || (nr2 == null || nr2 == "") || (nr3 == null || nr3 == "") || (nr1 <= 0 || nr2 <= 0 || nr3 <= 0)){
+    if(!isValid(nr1) || !isValid(nr2) || !isValid(nr3)){
         result.innerHTML = "Please, fill ALL fields with positive numbers!";
         result.style.color = "#ff0000";
-    }
-    else {
-        checkTriangle(nr1, nr2, nr3);
-        result.style.color = "#bc960b";
-        result.style.fontSize = "22px";
+        return;
+
     }
 
-    // Make sure the triangle exists 
-    function checkTriangle(a, b, c) {
-        if (a + b > c && b + c > a && a + c > b) {
-            getTypeTriangle(nr1, nr2, nr3);
-        }
-        else {
-            alert("Triangle does not exixt. TIP: The sum of 2 sides should be greater than the third side!");
-            return false;
-        }
+    if (! checkTriangle(nr1, nr2, nr3)) {
+        result.innerHTML = "Triangle does not exixt. TIP: The sum of 2 sides should be greater than the third side!";
+        return;
     }
+    
+    var triangleType = checkTriangle(nr1, nr2, nr3); 
+    result.innerHTML = triangleType;
+    result.style.color = "#bc960b";
+    result.style.fontSize = "22px";
+}
 
+// Make sure the triangle exists 
+function checkTriangle(a, b, c) {
+    return a + b > c && b + c > a && a + c > b;
 }
 
 
 //define and get the given triangle's type
 function getTypeTriangle(a, b, c) {
-
     if((a === b) && (b === c) && (c === a)) {
-        return result.innerHTML = "Equilateral";
+        return "Equilateral";
     } else if((a === b) || (b === c) || (c === a)) {
-        return result.innerHTML = "Isosceles";
+        return "Isosceles";
     } else if((a != b) && (b != c) && (c != a)) {
-        return result.innerHTML = "Scalene";
-    } else {
-        return result.innerHTML = "Error";  
+        return "Scalene";
     }
+}
+
+function isValid(elem) {
+    return !isNaN(elem) || !elem || elem <= 0
 }
 
 
